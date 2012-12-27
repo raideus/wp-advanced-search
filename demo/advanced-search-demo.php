6cfffd3e87116ce9221aeb76fb534d71a30c642d
Sean Butze <sbutze@gmail.com>
2012-12-26 23:49:14 -0500

    Updated array variable

--- demo/advanced-search-demo.php
+++ demo/advanced-search-demo.php
@@ -17,57 +17,57 @@ Page Attributes > Template.
 
 			<?php 
 			
-				$upf_filter = array();
+				$wpas = array();
 
-				$upf_filter['wp_query'] = array('post_type' => 'post',
+				$wpas['wp_query'] = array('post_type' => 'post',
 				                                'posts_per_page' => 5,
 				                                'tax_query' => array('relation' => 'OR'));
 
-				$upf_filter['fields'][] = array('type' => 'search',
+				$wpas['fields'][] = array('type' => 'search',
 				                                'title' => 'Search',
 				                                'std' => '');
 
 
-				$upf_filter['fields'][] = array('type' => 'taxonomy',
+				$wpas['fields'][] = array('type' => 'taxonomy',
 					                                'title' => 'Category',
 					                                'taxonomy' => 'category',
 					                                'format' => 'select',
 					                                'operator' => 'AND');
 
-				$upf_filter['fields'][] = array('type' => 'taxonomy',
+				$wpas['fields'][] = array('type' => 'taxonomy',
 					                                'title' => 'Tags',
 					                                'taxonomy' => 'post_tag',
 					                                'format' => 'checkbox',
 					                                'operator' => 'IN');
 
-				$upf_filter['fields'][] = array('type' => 'author',
+				$wpas['fields'][] = array('type' => 'author',
 				                                'title' => 'Author',
 				                                'format' => 'multi-select',
 				                                'authors' => array(1));
 
-				$upf_filter['fields'][] = array('type' => 'date',
+				$wpas['fields'][] = array('type' => 'date',
 				                                'title' => 'Year',
 				                                'date_type' => 'year',
 				                                'values' => array(),
 				                                'format' => 'select');
 
-				$upf_filter['fields'][] = array('type' => 'date',
+				$wpas['fields'][] = array('type' => 'date',
 				                                'title' => 'Month',
 				                                'date_type' => 'month',
 				                                'values' => array(),
 				                                'format' => 'select');
 
-				$upf_filter['fields'][] = array('type' => 'date',
+				$wpas['fields'][] = array('type' => 'date',
 				                                'title' => 'Day',
 				                                'date_type' => 'day',
 				                                'values' => array(),
 				                                'format' => 'select');
 
-				$upf_filter['fields'][] = array('type' => 'submit',
+				$wpas['fields'][] = array('type' => 'submit',
 				                                'value' => 'Search');
 
 
-				$event_filter = new WP_Advanced_Search($upf_filter);
+				$event_filter = new WP_Advanced_Search($wpas);
 
 				$event_filter->the_form();
 
